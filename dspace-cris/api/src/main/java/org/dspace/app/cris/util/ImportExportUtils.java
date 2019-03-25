e59f25016a96e667e2ad845425b8c987d6d5dd20
jonathan <jonathan.boss@uni-bamberg.de>
2019-03-25 14:55:21 +0100

    Fix export of nested objects

--- dspace-cris/api/src/main/java/org/dspace/app/cris/util/ImportExportUtils.java
+++ dspace-cris/api/src/main/java/org/dspace/app/cris/util/ImportExportUtils.java
@@ -245,6 +245,7 @@ public class ImportExportUtils {
         
 		// row index
 		int i = 1;
+		int ii = 1;
 		for (ACO rp : rps) {
 		    if(rp!=null) {	
     	        //HEADER_CRISID,HEADER_UUID,HEADER_SOURCEREF,HEADER_SOURCEID
@@ -276,7 +277,6 @@ public class ImportExportUtils {
     				}
     			}
     
-    			int ii = 1;
                 for (IContainable nestedContainable : metadataNestedLevel)
                 {
                     List<ACNO> nestedObject = applicationService
