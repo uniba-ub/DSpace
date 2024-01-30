3309cc4d2d94183e91f826d8f03169a5621419ca
Giuseppe Digilio <giuseppe.digilio@4science.com>
2024-01-30 18:10:08 +0100

    [DSC-1448] Fix sql query

--- dspace-api/src/main/resources/org/dspace/storage/rdbms/sqlmigration/h2/V8.0_2024.01.02__migrate-nbevents.sql
+++ dspace-api/src/main/resources/org/dspace/storage/rdbms/sqlmigration/h2/V8.0_2024.01.02__migrate-nbevents.sql
@@ -9,7 +9,7 @@
 -- --
 -- Copy raws from nbevent_processed table to qaevent_processed table
 -- --
-INSERT INTO qaevent_processed as qa (qaevent_id, qaevent_timestamp, eperson_uuid, item_uuid)
+INSERT INTO qaevent_processed (qaevent_id, qaevent_timestamp, eperson_uuid, item_uuid)
 SELECT nbevent_id, nbevent_timestamp, eperson_uuid, item_uuid UUID
 FROM nbevent_processed as nb WHERE NOT EXISTS(
 	SELECT * FROM qaevent_processed as qa WHERE qa.qaevent_id = nb.nbevent_id
