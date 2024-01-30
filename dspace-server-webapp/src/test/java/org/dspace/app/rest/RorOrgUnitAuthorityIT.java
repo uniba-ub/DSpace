eaa64c0cf66be2185067c696a60c4df08e189832
Andrea Bollini <andrea.bollini@4science.it>
2024-01-30 09:30:53 +0100

    DSC-1516 Expose country and country name as extra information. Allow to configure general ROR ORCID Authority properties leaving the option to customize them by plugin name

--- dspace-server-webapp/src/test/java/org/dspace/app/rest/RorOrgUnitAuthorityIT.java
+++ dspace-server-webapp/src/test/java/org/dspace/app/rest/RorOrgUnitAuthorityIT.java
@@ -8,7 +8,7 @@
 package org.dspace.app.rest;
 
 import static org.dspace.app.rest.matcher.ItemAuthorityMatcher.matchItemAuthorityWithOtherInformations;
-import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasItems;
 import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -32,16 +32,32 @@ public class RorOrgUnitAuthorityIT extends AbstractControllerIntegrationTest {
         expectedExtras.put("ror_orgunit_type", "Nonprofit");
         expectedExtras.put("data-ror_orgunit_acronym", "WEICan, IEEC");
         expectedExtras.put("ror_orgunit_acronym", "WEICan, IEEC");
+        expectedExtras.put("data-ror_orgunit_country", "CA");
+        expectedExtras.put("data-ror_orgunit_countryName", "CA");
+        expectedExtras.put("ror_orgunit_countryName", "CA");
+
+        Map<String, String> expectedExtras2 = new HashMap<>();
+        expectedExtras2.put("data-ror_orgunit_id", "https://ror.org/03vb2cr34");
+        expectedExtras2.put("ror_orgunit_id", "https://ror.org/03vb2cr34");
+        expectedExtras2.put("data-ror_orgunit_type", "Company");
+        expectedExtras2.put("ror_orgunit_type", "Company");
+        expectedExtras2.put("data-ror_orgunit_countryName", "Italia");
+        expectedExtras2.put("ror_orgunit_countryName", "Italia");
+        expectedExtras2.put("data-ror_orgunit_country", "IT");
 
         String token = getAuthToken(eperson.getEmail(), password);
         getClient(token).perform(get("/api/submission/vocabularies/OrgUnitAuthority/entries")
-            .param("filter", "test"))
+            .param("filter", "02z02cv32 OR 03vb2cr34"))
             .andExpect(status().isOk())
-            .andExpect(jsonPath("$._embedded.entries", hasSize(10)))
+            .andExpect(jsonPath("$._embedded.entries", hasSize(2)))
             .andExpect(jsonPath("$._embedded.entries",
-                hasItem(matchItemAuthorityWithOtherInformations("will be referenced::ROR-ID::https://ror.org/02z02cv32",
-                    "Wind Energy Institute of Canada", "Wind Energy Institute of Canada", "vocabularyEntry",
-                    expectedExtras))));
+                hasItems(
+                    matchItemAuthorityWithOtherInformations("will be referenced::ROR-ID::https://ror.org/02z02cv32",
+                            "Wind Energy Institute of Canada", "Wind Energy Institute of Canada", "vocabularyEntry",
+                            expectedExtras),
+                    matchItemAuthorityWithOtherInformations("will be referenced::ROR-ID::https://ror.org/03vb2cr34",
+                            "4Science", "4Science", "vocabularyEntry",
+                            expectedExtras2))));
     }
 
 }
