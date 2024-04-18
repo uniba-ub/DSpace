12ae5cb9da5c8ca90e05f9177151f119f29ae412
eskander <mohamed.eskander@4science.com>
2024-04-18 11:13:02 +0200

    [DSC-1600] added versioning check

--- dspace-api/src/main/java/org/dspace/content/ItemServiceImpl.java
+++ dspace-api/src/main/java/org/dspace/content/ItemServiceImpl.java
@@ -1924,17 +1924,37 @@ prevent the generation of resource policy entry values with null dspace_object a
 
     @Override
     public boolean canCreateNewVersion(Context context, Item item) throws SQLException {
+        boolean userAuthorized = false;
         if (authorizeService.isAdmin(context, item)) {
-            return true;
+            userAuthorized = true;
         }
 
         if (context.getCurrentUser() != null
-                && context.getCurrentUser().equals(item.getSubmitter())) {
-            return configurationService.getPropertyAsType(
-                    "versioning.submitterCanCreateNewVersion", false);
+            && context.getCurrentUser().equals(item.getSubmitter())) {
+            userAuthorized = configurationService.getPropertyAsType(
+                "versioning.submitterCanCreateNewVersion", false);
         }
 
-        return false;
+        if (!userAuthorized) {
+            List<String> allowedGroups = List
+                    .of(configurationService.getArrayProperty("versioning.allowed.groups"));
+            userAuthorized = allowedGroups.stream().anyMatch(groupName -> {
+                try {
+                    return groupService.isMember(context, groupName);
+                } catch (SQLException e) {
+                    return false;
+                }
+            });
+        }
+
+        if (userAuthorized) {
+            List<String> allowedEntities = List
+                .of(configurationService.getArrayProperty("versioning.enabled.entities"));
+            String entityType = getEntityType(item);
+            return entityType != null && allowedEntities.contains(entityType);
+        }
+
+        return userAuthorized;
     }
 
     /**
