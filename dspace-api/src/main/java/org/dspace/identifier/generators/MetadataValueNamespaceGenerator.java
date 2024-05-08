b7b6e0e7928984def87f502b0e449439d3d56eed
Vincenzo Mecca <vincenzo.mecca@4science.com>
2024-05-08 14:12:20 +0200

    [DSC-1075] fixes and support multiple metadata fields

--- dspace-api/src/main/java/org/dspace/identifier/generators/MetadataValueNamespaceGenerator.java
+++ dspace-api/src/main/java/org/dspace/identifier/generators/MetadataValueNamespaceGenerator.java
@@ -9,10 +9,13 @@
 package org.dspace.identifier.generators;
 
 import java.util.Optional;
+import java.util.Set;
 
+import org.apache.commons.lang3.StringUtils;
 import org.dspace.content.Item;
 import org.dspace.content.service.ItemService;
 import org.dspace.core.Context;
+import org.dspace.services.ConfigurationService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
@@ -24,31 +27,42 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 public class MetadataValueNamespaceGenerator implements DoiNamespaceGenerator {
 
-    private String metadataField;
-    private String postValue;
-
+    protected final Set<String> metadataFields;
+    protected final String postValue;
+    protected final String namespaceSeparator;
     @Autowired(required = true)
     private transient ItemService itemService;
+    @Autowired(required = true)
+    private ConfigurationService configurationService;
+
+    MetadataValueNamespaceGenerator(
+        String namespaceSeparator,
+        String postValue,
+        Set<String> metadataFields
+    ) {
+        this.namespaceSeparator = namespaceSeparator;
+        this.postValue = postValue;
+        this.metadataFields = metadataFields;
+    }
 
     @Override
     public String getNamespace(Context context, Item item) {
-        return itemService.getMetadata(item, metadataField) + Optional.ofNullable(postValue).orElse("");
+        for (String metadataField : metadataFields) {
+            String metadataValue = itemService.getMetadata(item, metadataField);
+            if (StringUtils.isBlank(metadataValue)) {
+                continue;
+            }
+            return getFormattedNamespace(metadataValue);
+        }
+        return getFormattedNamespace(namespaceSeparator);
     }
 
-    public String getMetadataField() {
-        return metadataField;
+    private String getFormattedNamespace(String prefix) {
+        return prefix + getPostValue();
     }
 
-    public void setMetadataField(String metadataField) {
-        this.metadataField = metadataField;
-    }
-
-    public String getPostValue() {
-        return postValue;
-    }
-
-    public void setPostValue(String postValue) {
-        this.postValue = postValue;
+    private String getPostValue() {
+        return Optional.ofNullable(postValue).orElse("");
     }
 
 }
