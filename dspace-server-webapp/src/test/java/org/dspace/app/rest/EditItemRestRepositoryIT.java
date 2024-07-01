9ae813e90204962fcb20e59509801df623f80466
Giuseppe Digilio <giuseppe.digilio@4science.com>
2024-07-01 11:43:44 +0200

    [DSC-1294] Fix missing import

--- dspace-server-webapp/src/test/java/org/dspace/app/rest/EditItemRestRepositoryIT.java
+++ dspace-server-webapp/src/test/java/org/dspace/app/rest/EditItemRestRepositoryIT.java
@@ -58,6 +58,7 @@ import org.dspace.eperson.Group;
 import org.dspace.eperson.service.GroupService;
 import org.dspace.services.ConfigurationService;
 import org.hamcrest.Matchers;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mock.web.MockMultipartFile;
