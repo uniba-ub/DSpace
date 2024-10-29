86bc482b616de864f23e68f7b4c316cfe8017a60
Adamo <adamo.fapohunda@4science.com>
2024-10-29 10:26:08 +0100

    [DSC-1997] Fixed license header

--- dspace-server-webapp/src/test/java/org/dspace/app/rest/health/EPersonGroupHealthIndicatorTest.java
+++ dspace-server-webapp/src/test/java/org/dspace/app/rest/health/EPersonGroupHealthIndicatorTest.java
@@ -1,3 +1,10 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
 package org.dspace.app.rest.health;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -22,6 +29,9 @@ import org.springframework.boot.actuate.health.Status;
 
 /**
  * Unit tests for {@link EPersonGroupHealthIndicator}.
+ *
+ * @author Adamo Fapohunda (adamo.fapohunda at 4science.com)
+ *
  */
 public class EPersonGroupHealthIndicatorTest extends AbstractUnitTest {
 
--- dspace-server-webapp/src/test/java/org/dspace/app/rest/health/SiteHealthIndicatorTest.java
+++ dspace-server-webapp/src/test/java/org/dspace/app/rest/health/SiteHealthIndicatorTest.java
@@ -1,3 +1,10 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
 package org.dspace.app.rest.health;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -23,6 +30,9 @@ import org.springframework.boot.actuate.health.Status;
 
 /**
  * Unit tests for {@link SiteHealthIndicator}.
+ *
+ * @author Adamo Fapohunda (adamo.fapohunda at 4science.com)
+ *
  */
 public class SiteHealthIndicatorTest extends AbstractUnitTest {
 
